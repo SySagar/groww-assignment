f0c22df3eba125b843f072782f3ac9ed5b21cd56
SySagar <sysagar07@gmail.com>
2024-03-13 21:20:37 +0530

    style: dynamic css variables

--- app/layout/Header/Header.jsx
+++ app/layout/Header/Header.jsx
@@ -1,30 +1,54 @@
-'use client'
-import React from 'react';
-import { useThemeStore } from '@/app/store/themeStore';
-import style from './header.module.css';
-import Image from 'next/image'
-import useThemeData from '@/app/hooks/useThemeData';
+"use client";
+import React, { useEffect } from "react";
+import { useMerchantStore } from "@/app/store/merchantStore";
+import themeChanger from "@/app/theme/themeChanger";
+import APIMethods from "@lib/api";
+import style from "./header.module.css";
+import Image from "next/image";
 
 export default function Header() {
-  const { merchantName, merchantLogo } = useThemeStore();
-  const { theme } = useThemeData();
+  const { merchantName, merchantLogo, setMerchant } = useMerchantStore();
+  const { fetchThemeData, resetThemeToOriginal } = themeChanger();
 
-  console.log('merchantLogo', merchantLogo);
+  const themeChange = () => {
+    fetchThemeData();
+  };
+
+  const defaultTheme = () => {
+    resetThemeToOriginal();
+  };
+
+  const fethcMerchantDetails = async () => {
+    const response = await APIMethods.merchant.data();
+    const merchantLogo = response.data.merchantLogo;
+    const merchantName = response.data.merchantName;
+    setMerchant({ merchantName, merchantLogo });
+  }
+
+  useEffect(() => {
+    fethcMerchantDetails();
+  }, []);
 
   return (
     <div className={style.headerContainer}>
       <div className={style.headerBody}>
-        
-      <div className={style.logoContainer}>
-      <Image
-      src={merchantLogo}
-      width={40}
-      height={40}
-      alt="Picture of the author"
-    />
-      </div>
-      
+        <div className={style.logoContainer}>
+          <Image
+            src={merchantLogo}
+            width={35}
+            height={35}
+            alt="Picture of the author"
+          />
+          <p>{merchantName} Cart</p>
+        </div>
+
+        <div className={style.themeToggle}>
+          <button onClick={defaultTheme} className={style.button}>default</button>
+          <button onClick={themeChange} className={style.button2}>
+            custom
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
---
+++ app/store/merchantStore.js
@@ -0,0 +1,14 @@
+import create from 'zustand';
+
+const initialState = {
+  merchantName: '',
+  merchantLogo: '',
+};
+
+export const useMerchantStore = create((set) => ({
+  ...initialState,
+  setMerchant: (data) => set((state) => ({
+    merchantName: data.merchantName,
+    merchantLogo: data.merchantLogo,
+  })),
+}));
