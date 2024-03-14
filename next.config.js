27e2bd6af219e6c19291c5d812aa80ded8a223d2
SySagar <sysagar07@gmail.com>
2024-03-14 23:59:31 +0530

    chore: next config absolute path

--- next.config.js
+++ next.config.js
@@ -10,6 +10,18 @@ const nextConfig = {
         },
       ],
     },
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, './'),
+        "@components": path.resolve(__dirname, './app/components'),
+        "@layout": path.resolve(__dirname, './app/layout'),
+        "@store": path.resolve(__dirname, './app/store'),
+        "@lib": path.resolve(__dirname, './app/lib'),
+      };
+  
+      return config;
+    },
   };
   
   module.exports = nextConfig;
